16acd084e18ccb770cf37373bd614cfc63a17701
Randy Straka <rstraka@paypal.com>
2017-07-17 12:38:34 -0500

    Updating to ask for user authorization id

--- Samples/Source/GetAuthorization.aspx.cs
+++ Samples/Source/GetAuthorization.aspx.cs
@@ -23,7 +23,7 @@ namespace PayPal.Sample
             // a request id if you do not pass one explicitly. 
             // See [Configuration.cs](/Source/Configuration.html) to know more about APIContext.
             var apiContext = Configuration.GetAPIContext();
-            var authorizationId = "2BR94342X1608434M";
+            var authorizationId = "<your authorization id here>";
 
             #region Track Workflow
             //--------------------
